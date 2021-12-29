bce5cc79ec1758ad04742fc33593824cf577d092
EC2 Default User <ec2-user@ip-172-31-41-143.us-east-2.compute.internal>
2021-12-29 04:16:02 +0000

    電卓

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 let result = document.getElementById("result");
 
+function calc_btn(calc) {
+
+ if (result.value.slice(-1) == "+" || "-" || "*" || "/") {
+  const answer = result.value + calc.value;
+  const footSlice = answer.slice(0, -1);
+  const display = footSlice + calc.value;
+  console.log(display);
+  }
+}
+
 function num_btn(num) {
-  
  if (result.value == "0") {
    result.value = num.value;
   } else {
    result.value += num.value;
-  }
-}
-
-function calc_btn(calc) {
-  
-  if (result.value.slice(-1) == "+" || "-" || "*" || "/") {
-    const f = result.value.slice(0, -1);
-    result.value = f + calc.value;
-  } else {
-    result.value += calc.value;
   }
 }
 
